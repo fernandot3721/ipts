cad2f92f7efb6c77510b483648f891fcc293bef2
fernandot <ft@producec.com>
2014-08-30 11:01:28 +0800

    地图信息更新

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
-    var latitudes = [38.885516, 38.898537, 38.8507126, 38.84753, 39, 40, 41, 29.119783];
-    var longitudes = [-77.09327200000001, -77.13208299999997, -77.09903600000001, -77.06577290000001, 1, 2, 3, -113.350771];
+    var latitudes = [40.753701, 22.303624, 23.124102, 30.544562, 22.545571, 39.908491, 31.231452, 41.814823];
+    var longitudes = [-73.972024, 114.160231, 113.323479, 114.335544, 114.055398, 116.447055, 121.477726, 123.435958];
     function init_map(index) {
         var myLocation = new google.maps.LatLng(latitudes[index-1], longitudes[index-1]);
         var mapOptions = {
@@ -15,7 +15,7 @@ jQuery(function ($) {
             mapOptions);
         marker.setMap(map);
     }
-   
+
     init_map(1);
     init_map(2);
     init_map(3);
